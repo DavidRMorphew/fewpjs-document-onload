fc985becfb44aa8552f5215cf945deb61b9253fb
DavidRMorphew <morphewd@umich.edu>
2021-03-19 12:57:32 -0400

    commit tree 48973f5d2e4739c2d49d603c258412165a48a3b8

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+const textForP = document.getElementById('text')
+document.addEventListener('DOMContentLoaded', function() {
+    textForP.innerHTML = "This is really cool!";
+})
+
